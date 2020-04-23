3fb8d5fcaa9de52061e5caa9e2751a75bdfda578
Diana <diana.amfreitas@gmail.com>
2020-04-23 14:13:11 +0100

    Added Helicopter Controller

---
+++ src/src/main/java/com/lpoo/g72/controller/HelicopterController.java
@@ -0,0 +1,78 @@
+package com.lpoo.g72.controller;
+import com.lpoo.g72.command.Command;
+import com.lpoo.g72.command.DownCommand;
+import com.lpoo.g72.command.RightCommand;
+import com.lpoo.g72.command.UpCommand;
+import com.lpoo.g72.gui.Gui;
+import com.lpoo.g72.scene.Element;
+import com.lpoo.g72.scene.Position;
+import com.lpoo.g72.scene.Scene;
+
+import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
+
+public class HelicopterController {
+    private  Gui gui;
+    private Scene scene;
+    int altitude;
+    long velocity;
+    Instant start;
+
+    public HelicopterController(Gui gui, Scene scene){
+        this.scene = scene;
+        this.gui = gui;
+        this.altitude = scene.getHelicopter().getPosition().getY();
+        this.velocity = 200000000;
+        this.start = Instant.now();
+    }
+
+    public void executeCommand() throws IOException {
+        Command cmd;
+        Gui.Key key = gui.getKey();
+        this.moveForward();
+
+        if(key == Gui.Key.DOWN && isWithinDownLimit()){
+            cmd = new DownCommand(scene.getHelicopter());
+            cmd.execute();
+        }
+        else if(key == Gui.Key.UP && isWithinUpLimit()){
+            cmd = new UpCommand(scene.getHelicopter());
+            cmd.execute();
+        }
+    }
+
+    boolean newRound(){
+        return scene.getHelicopter().getPosition().getX() == scene.getWidth()-1;
+    }
+
+    void moveForward(){
+
+        Instant current = Instant.now();
+        Duration timePassed = Duration.between(start , current);
+
+        if(newRound()){
+            decreaseAltitude(scene.getHelicopter());
+            altitude++;
+            start = Instant.now();
+        }
+
+        if(timePassed.getNano() >= velocity){
+            Command right = new RightCommand(scene.getHelicopter());
+            right.execute();
+            start = current;
+        }
+    }
+
+    void decreaseAltitude(Element element){
+        element.setPosition(new Position(0,altitude + 1));
+    }
+
+    boolean isWithinUpLimit(){
+        return scene.getHelicopter().getPosition().getY() - 1 > altitude - 1;
+    }
+
+    boolean isWithinDownLimit(){
+        return scene.getHelicopter().getPosition().getY() - 1 < altitude + 2;
+    }
+}
--- src/src/main/java/com/lpoo/g72/controller/SceneController.java
+++ src/src/main/java/com/lpoo/g72/controller/SceneController.java
@@ -6,6 +6,7 @@ import com.lpoo.g72.command.UpCommand;
 import com.lpoo.g72.creator.LisbonScene;
 import com.lpoo.g72.gui.Gui;
 import com.lpoo.g72.scene.Element;
+import com.lpoo.g72.scene.Helicopter;
 import com.lpoo.g72.scene.Position;
 import com.lpoo.g72.scene.Scene;
 import java.io.IOException;
@@ -16,26 +17,13 @@ public class SceneController{
     private final Gui gui;
     private Scene scene;
 
+    private HelicopterController helicopterController;
+
     public SceneController(Gui gui){
         this.gui = gui;
         // The scene should be set in the menu then when the user chooses the city
         this.scene = new LisbonScene().createScene(gui.getWidth(),gui.getHeight());
-    }
-
-    public void executeAction(int previousAltitude) throws IOException {
-        Command cmd;
-        Gui.Key key = gui.getKey();
-
-        if(key == Gui.Key.DOWN && isWithinDownLimit(previousAltitude)){
-            cmd = new DownCommand(scene.getHelicopter());
-            cmd.execute();
-        }
-        else if(key == Gui.Key.UP && isWithinUpLimit(previousAltitude)){
-            cmd = new UpCommand(scene.getHelicopter());
-            cmd.execute();
-        }
-
-        gui.drawScene(scene);
+        this.helicopterController = new HelicopterController(gui,scene);
     }
 
     public void start() throws IOException {
@@ -45,48 +33,9 @@ public class SceneController{
 
     public void run() throws IOException {
 
-        Instant start = Instant.now(), current;
-        Duration duration;
-        long interval = 200000000;
-
-        int previousAltitude = scene.getHelicopter().getPosition().getY();
-
         while(true){
-            current = Instant.now();
-            duration = Duration.between(start , current);
-
-            if(newRound()){
-                decreaseAltitude(scene.getHelicopter(), previousAltitude);
-                previousAltitude++;
-            }
-
-            if(duration.getNano() >= interval){
-                moveHelicopterForward();
-                start = current;
-            }
-
-            executeAction(previousAltitude);
+            helicopterController.executeCommand();
+            gui.drawScene(scene);
         }
-    }
-
-    boolean newRound(){
-        return scene.getHelicopter().getPosition().getX() == scene.getWidth()-1;
-    }
-
-    boolean isWithinUpLimit(int previousAltitude){
-        return scene.getHelicopter().getPosition().getY() - 1 > previousAltitude - 1;
-    }
-
-    boolean isWithinDownLimit(int previousAltitude){
-        return scene.getHelicopter().getPosition().getY() - 1 < previousAltitude + 2;
-    }
-
-    void moveHelicopterForward(){
-        Command right = new RightCommand(scene.getHelicopter());
-        right.execute();
-    }
-
-    void decreaseAltitude(Element element, int previousAltitude){
-        element.setPosition(new Position(0,previousAltitude + 1));
     }
 }
