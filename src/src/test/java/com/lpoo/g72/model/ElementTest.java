7eb28d37a60ab05b1309c22eb653999b06addbd2
Eduardo Brito <developer.edurbrito@gmail.com>
2020-05-01 16:43:53 +0100

    ModelTest & ElementTest

--- src/src/test/java/com/lpoo/g72/model/ElementTest.java
+++ src/src/test/java/com/lpoo/g72/model/ElementTest.java
@@ -2,6 +2,7 @@ package com.lpoo.g72.model;
 
 import com.lpoo.g72.model.element.Element;
 import com.lpoo.g72.model.element.Helicopter;
+import com.lpoo.g72.model.element.Monster;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -15,5 +16,12 @@ public class ElementTest {
 
         element.setPosition(new Position(15,16));
         assertEquals(true, element.getPosition().equals(new Position(15,16)));
+
+        element = new Monster(new Position(31,22));
+
+        assertEquals(true, element.getPosition().equals(new Position(31,22)));
+
+        element.setPosition(new Position(19,95));
+        assertEquals(true, element.getPosition().equals(new Position(19,95)));
     }
 }
